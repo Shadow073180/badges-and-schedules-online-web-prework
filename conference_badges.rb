3e85650b5bdd44af491d4940a3f87e4937eca673
David masterson <david73180@gmail.com>
2018-09-19 02:12:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,9 @@ end
 def batch_badge_creator(array)
   array2 = []
   array.each do |name|
-    return "Hello, my name is #{name}."
-    array2.push(name)
+  array2.push("Hello, my name is #{name}.")
   end
+    return (array2)
   
 end
 
