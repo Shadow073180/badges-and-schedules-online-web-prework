300dbb5e2fc2375b263b26ec52ef928bb8b6a8e4
David masterson <david73180@gmail.com>
2018-09-19 03:15:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(array)
   array2 = []
   array1 = ["1", "2", "3", "4", "5", "6", "7"]
   array.each do |name|
-   array2[i] = "Hello #{name}! You'll be assigned to room #{array1[i]}!"
+   array2[i] = "Hello, #{name}! You'll be assigned to room #{array1[i]}!"
   i += 1
 end
   return array2
